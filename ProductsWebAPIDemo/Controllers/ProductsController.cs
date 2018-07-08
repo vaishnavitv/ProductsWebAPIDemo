66f3974f39d9d22e5a03f670866e575db9290687
Vaishnavi T V <vaishnavi.subramanian@gmail.com>
2018-07-07 21:21:46 -0700

    First ASP.Net Web API Sample.

---
+++ ProductsWebAPIDemo/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using ProductsWebAPIDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductsWebAPIDemo.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        List<Product> products = new List<Product>()
+        {
+            new Product{ Id = 1, Name = "Hardware", Category = "Hardware", Price= 600},
+            new Product{ Id = 2, Name = "Mouse", Category = "Peripherals", Price= 20},
+            new Product{ Id = 3, Name = "Monitor", Category = "Peripherals", Price= 200},
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+        public IHttpActionResult GetProduct(int id)
+        {
+           Product product = products.FirstOrDefault<Product>(p => p.Id.Equals(id));
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+    }
+}
---
+++ ProductsWebAPIDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProductsWebAPIDemo.WebApiApplication" Language="C#" %>
